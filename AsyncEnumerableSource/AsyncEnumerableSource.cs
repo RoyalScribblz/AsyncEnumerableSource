5370701456018691286512a75df21e39a80de263
RoyalScribblz <clashmonster123@gmail.com>
2025-03-07 10:37:20 +0000

    added support for netstandard2.1+

--- AsyncEnumerableSource/AsyncEnumerableSource.cs
+++ AsyncEnumerableSource/AsyncEnumerableSource.cs
@@ -1,170 +1,177 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Channels;
+using System.Threading.Tasks;
 
-namespace AsyncEnumerableSource;
-
-public abstract class AsyncEnumerableSource
+namespace AsyncEnumerableSource
 {
-    protected static readonly UnboundedChannelOptions ChannelOptions = new()
+    public abstract class AsyncEnumerableSource
     {
-        SingleWriter = true,
-        SingleReader = true,
-    };
-}
+        protected static readonly UnboundedChannelOptions ChannelOptions = new UnboundedChannelOptions()
+        {
+            SingleWriter = true,
+            SingleReader = true,
+        };
+    }
 
-public sealed class AsyncEnumerableSource<T> : AsyncEnumerableSource
-{
-    private readonly List<Channel<T>> _channels = [];
-    private readonly ReaderWriterLockSlim _lock = new();
-    private bool _completed;
-    private Exception? _exception;
-
-    public async IAsyncEnumerable<T> GetAsyncEnumerable(
-        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    public sealed class AsyncEnumerableSource<T> : AsyncEnumerableSource
     {
-        if (_exception != null)
-        {
-            throw _exception;
-        }
-
-        if (_completed)
-        {
-            yield break;
-        }
-
-        var channel = Channel.CreateUnbounded<T>(ChannelOptions);
+        private readonly List<Channel<T>> _channels = new List<Channel<T>>();
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        private int _completed;
+        private Exception _exception;
         
-        _lock.EnterWriteLock();
-        try
-        {
-            _channels.Add(channel);
-        }
-        finally
-        {
-            _lock.ExitWriteLock();
-        }
+        private const int True = 1;
         
-        try
+        public async IAsyncEnumerable<T> GetAsyncEnumerable(
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
-            // https://learn.microsoft.com/en-us/dotnet/api/system.threading.channels.channelreader-1.readallasync?view=net-9.0#parameters
-            await foreach (var item in channel.Reader.ReadAllAsync(CancellationToken.None).ConfigureAwait(false))
+            if (_exception != null)
             {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    yield break;
-                }
-                
-                yield return item;
+                throw _exception;
             }
-        }
-        finally
-        {
+
+            if (_completed == True)
+            {
+                yield break;
+            }
+
+            var channel = Channel.CreateUnbounded<T>(ChannelOptions);
+        
             _lock.EnterWriteLock();
             try
             {
-                _channels.Remove(channel);
+                _channels.Add(channel);
             }
             finally
             {
                 _lock.ExitWriteLock();
             }
-        }
-    }
-
-    public void YieldReturn(T value)
-    {
-        if (_completed)
-        {
-            return;
-        }
-
-        List<Channel<T>> channelsSnapshot;
-        _lock.EnterReadLock();
-        try
-        {
-            channelsSnapshot = [.._channels];
-        }
-        finally
-        {
-            _lock.ExitReadLock();
-        }
         
-        if (channelsSnapshot.Count >= 50)
-        {
-            Parallel.ForEach(channelsSnapshot, channel => channel.Writer.TryWrite(value));
-        }
-        else
-        {
-            foreach (var channelsKey in channelsSnapshot)
+            try
             {
-                channelsKey.Writer.TryWrite(value);
+                // https://learn.microsoft.com/en-us/dotnet/api/system.threading.channels.channelreader-1.readallasync?view=net-9.0#parameters
+                await foreach (var item in channel.Reader.ReadAllAsync(CancellationToken.None).ConfigureAwait(false))
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        yield break;
+                    }
+                
+                    yield return item;
+                }
+            }
+            finally
+            {
+                _lock.EnterWriteLock();
+                try
+                {
+                    _channels.Remove(channel);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
             }
         }
-    }
 
-    public void Complete()
-    {
-        if (Interlocked.Exchange(ref _completed, true))
+        public void YieldReturn(T value)
         {
-            return;
-        }
-
-        List<Channel<T>> channelsSnapshot;
-        _lock.EnterReadLock();
-        try
-        {
-            channelsSnapshot = [.._channels];
-        }
-        finally
-        {
-            _lock.ExitReadLock();
-        }
-        
-        if (_channels.Count >= 50)
-        {
-            Parallel.ForEach(channelsSnapshot, channel => channel.Writer.TryComplete());
-        }
-        else
-        {
-            foreach (var channelsKey in channelsSnapshot)
+            if (_completed == True)
             {
-                channelsKey.Writer.TryComplete();
+                return;
+            }
+
+            List<Channel<T>> channelsSnapshot;
+            _lock.EnterReadLock();
+            try
+            {
+                channelsSnapshot = new List<Channel<T>>(_channels);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        
+            if (channelsSnapshot.Count >= 50)
+            {
+                Parallel.ForEach(channelsSnapshot, channel => channel.Writer.TryWrite(value));
+            }
+            else
+            {
+                foreach (var channelsKey in channelsSnapshot)
+                {
+                    channelsKey.Writer.TryWrite(value);
+                }
             }
         }
-    }
 
-    public void Fault(Exception error)
-    {
-        if (Interlocked.CompareExchange(ref _exception, error, null) != null)
+        public void Complete()
         {
-            return;
-        }
-
-        if (Interlocked.Exchange(ref _completed, true))
-        {
-            return;
-        }
-        
-        List<Channel<T>> channelsSnapshot;
-        _lock.EnterReadLock();
-        try
-        {
-            channelsSnapshot = [.._channels];
-        }
-        finally
-        {
-            _lock.ExitReadLock();
-        }
-
-        if (_channels.Count >= 50)
-        {
-            Parallel.ForEach(channelsSnapshot, channel => channel.Writer.TryComplete(error));
-        }
-        else
-        {
-            foreach (var channelsKey in channelsSnapshot)
+            if (Interlocked.Exchange(ref _completed, True) == True)
             {
-                channelsKey.Writer.TryComplete(error);
+                return;
+            }
+
+            List<Channel<T>> channelsSnapshot;
+            _lock.EnterReadLock();
+            try
+            {
+                channelsSnapshot = new List<Channel<T>>(_channels);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        
+            if (_channels.Count >= 50)
+            {
+                Parallel.ForEach(channelsSnapshot, channel => channel.Writer.TryComplete());
+            }
+            else
+            {
+                foreach (var channelsKey in channelsSnapshot)
+                {
+                    channelsKey.Writer.TryComplete();
+                }
+            }
+        }
+
+        public void Fault(Exception error)
+        {
+            if (Interlocked.CompareExchange(ref _exception, error, null) != null)
+            {
+                return;
+            }
+
+            if (Interlocked.Exchange(ref _completed, True) == True)
+            {
+                return;
+            }
+        
+            List<Channel<T>> channelsSnapshot;
+            _lock.EnterReadLock();
+            try
+            {
+                channelsSnapshot = new List<Channel<T>>(_channels);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+
+            if (_channels.Count >= 50)
+            {
+                Parallel.ForEach(channelsSnapshot, channel => channel.Writer.TryComplete(error));
+            }
+            else
+            {
+                foreach (var channelsKey in channelsSnapshot)
+                {
+                    channelsKey.Writer.TryComplete(error);
+                }
             }
         }
     }
